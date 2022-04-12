a39bba56a76fec41f8ad64b0e2d10574722db0c0
idan <idanmagled@gmail.com>
2022-04-12 19:28:24 +0300

    Init

---
+++ install.sh
@@ -0,0 +1,5 @@
+sudo apt update
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh
+sudo apt install nodejs
+git clone git@github.com:blackcolt/MongoDB-OrangePI.git .
+bash install.sh

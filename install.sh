d5b060eb1250c5ca9693ecfbb4f53a2d56e48126
idan <idanmagled@gmail.com>
2022-04-13 01:34:11 +0300

    - Fixed path.

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 sudo apt update
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh
 sudo apt install nodejs
-git clone git@github.com:blackcolt/MongoDB-OrangePI.git .
+git clone git@github.com:blackcolt/MongoDB-OrangePI.git
+cd MongoDB-OrangePI
 bash install.sh

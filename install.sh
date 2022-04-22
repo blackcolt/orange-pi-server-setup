2ea22e2bce77731855d95684b2c8f5590e53aa67
Idan-Magled <idanmagled@gmail.com>
2022-04-23 02:58:48 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,5 +6,6 @@ sudo apt install nodejs
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 nvm use v16.14.2
 git clone git@github.com:blackcolt/MongoDB-OrangePI.git
+source ~/.zshrc
 cd MongoDB-OrangePI || exit
 bash install.sh

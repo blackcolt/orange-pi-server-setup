3237ca39141b41d990f6d100e5ed0b8e77dbf507
idan.magled <idanmagled@gmail.com>
2022-04-23 02:30:46 +0300

    - Fixed install

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
+# need to use bullsye
+#mongoose - 6.0.12
 sudo apt update
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh
 sudo apt install nodejs
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+nvm use v16.14.2
 git clone git@github.com:blackcolt/MongoDB-OrangePI.git
-cd MongoDB-OrangePI
+cd MongoDB-OrangePI || exit
 bash install.sh

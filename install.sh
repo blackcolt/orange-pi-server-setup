ea6cee0aff98b17d33f6510782d58b2f3065bc58
idan <idanmagled@gmail.com>
2023-12-23 15:14:44 +0200

    - git ignore

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@ sudo apt update
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh
 sudo apt install nodejs
 git clone git@github.com:blackcolt/MongoDB-OrangePI.git
-cd MongoDB-OrangePI
+cd MongoDB-OrangePI || exit
 bash install.sh
